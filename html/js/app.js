133ce04e69b655bda66f8a54871fc7def6cca89a
Ryan Harvey <harvey6@stolaf.edu>
2023-08-03 17:09:16 -0500

    State selection and city box functionality added

--- html/js/app.js
+++ html/js/app.js
@@ -1,55 +1,40 @@
 d3.csv("uscities.csv").then(function (data) {
-  // console.log(data);
-
   var cityStates = data;
 
-  var button = d3.select("#selectState");
+  var button = d3.select("#selectState"); //The select state box
+  button.on("click",setStates) //Whenever you click on the states box
+  button.on("change", runEnter); //Whenver you change the states box
 
-  //var form = d3.select("#form");
-
-  button.on("change", runEnter);
-  //form.on("submit", runEnter);
-
-  console.log(cityStates)
+  hasRan = false //Only run once to prevent lag
+  function setStates() {
+    if(hasRan == false){
+      filteredCities = data.sort(function (a,b) {return d3.ascending(a.State, b.State);}); //Sort states alphabetically
+      hasRan = true
+      d3.event.preventDefault(); //stops reload
+      arr = []
+      for (var i = 0; i<data.length;i++) { //Get all unique state names and put them in arr
+        curState = data[i].State
+        if(!(arr.includes(curState))){ //If curState not in arr
+          arr.push(data[i].State); //Append to arr
+          d3.select('#selectState').insert("option").html(curState)
+        }
+      }
+    }
+  }
 
   function runEnter() {
-
-    d3.select("output").html("") //goes to the output at end of file, will change
+    d3.select("#selectCity").html("")//clears the list
     d3.event.preventDefault(); //stops page refreshing
     var inputElement = d3.select("#selectState").property("value"); //Input from the state select box
-    console.log(typeof(inputElement))
-    var filteredCities = cityStates.filter(cityStates => cityStates.State.includes(inputElement))
-    console.log(filteredCities)
-    /*
-    d3.select("output").html("")
-    d3.event.preventDefault();
-    var inputElement = d3.select("#selectState");
-    var inputValue = inputElement.property("value").toLowerCase().trim();
+    if(inputElement!='---'){
+      var filteredCities = cityStates.filter(cityStates => cityStates.State.includes(inputElement)) //filters data for all cities with the given state
+      filteredCities.sort(function (a,b) {return d3.ascending(a.City, b.City);});  //Sort cities alphabetically
 
-    console.log(inputValue.length)
-
-    // console.log(inputValue.length);
-    // console.log(movies);
-    /*
-    if (inputValue.length < 6){
-      d3.select("p").classed('noresults2', true).html("<center><strong>Please try using more than 5 characters to avoid too many results!</strong>")
-      inputValue = "Something to give no results"
+      for (var i = 0; i<filteredCities.length;i++) { 
+        city = filteredCities[i].City
+        d3.select('#selectCity').insert("option").html(city)
+      }
     }
-    var filteredData = movies.filter(movies => movies.actors.toLowerCase().trim().includes(inputValue));
-    // console.log(filteredData.length)
-    if (filteredData.length === 0 && inputValue !== "Something to give no results"){
-      d3.select("p").classed('noresults', true).html("<center><strong>No results. Please check your spelling!</strong>")
-    }
-    output = _.sortBy(filteredData, 'avg_vote').reverse()
-
-    for (var i = 0; i < filteredData.length; i++) {
-      // console.log(output[i]['original_title'])
-      // console.log(output[i]['avg_vote'])
-      // d3.select("tbody>tr>td").text(output[i]['original_title']);
-      d3.select("tbody").insert("tr").html("<td>"+[i+1]+"</td>"+"<td>"+"<a href=https://www.imdb.com/title/"+output[i]['imdb_title_id']+" target='_blank'>"+(output[i]['original_title'])+"</a>"
-      + "</td>" +"<td>" +(output[i]['avg_vote'])+"</td>" +"<td>" +(output[i]['year'])+"</td>"  +"<td>" +(output[i]['director'])+"</td>"+"<td>" +(output[i]['description'])+"</td>") }
-      */
   };
-
 
 });
